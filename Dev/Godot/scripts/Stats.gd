22435988015a2db17c546ef5c3a396571250173a
ueshita <nalto32@gmail.com>
2022-09-25 01:50:34 +0900

    Add stats display

---
+++ Dev/Godot/scripts/Stats.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready():
+	pass
+
+func _process(delta: float):
+	$FPS.text = "FPS: %.3f" % Performance.get_monitor(Performance.TIME_FPS)
+	$DrawCalls.text = "DrawCalls: %.0f" % Performance.get_monitor(Performance.RENDER_TOTAL_DRAW_CALLS_IN_FRAME)
+	$Objects.text = "Objects: %.0f" % Performance.get_monitor(Performance.RENDER_TOTAL_OBJECTS_IN_FRAME)
+	$Primitives.text = "Primitives: %.0f" % Performance.get_monitor(Performance.RENDER_TOTAL_PRIMITIVES_IN_FRAME)
